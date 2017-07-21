ff4fd12c72a7f2233ac24ef19d5d75e9d7bd03b2
Derta <derta.isyajora@gmail.com>
2017-07-21 21:30:34 +0700

    Clean up routes

--- routes/web.php
+++ routes/web.php
@@ -15,25 +15,31 @@ $app->get('/', function () use ($app) {
     return $app->version();
 });
 
-
+// Get API Version
 $app->get('api/version','BranchController@version');
+
+// Get nearest Barclays ATM or Branch 
 $app->post('api/nearest_branch','BranchController@nearest');
-// direction customized from Google Maps API
+
+// Get customized direction based on Google Maps API
 $app->post('api/direction','DirectionController@get_direction');
 
-// request Uber from Uber API
+// Request Uber ride from Uber API
 $app->post('api/request_uber','UberController@request_uber');
 
+// Save user token after give authorization to BarclaysEye App
+$app->get('api/redirect_uri','UberController@redirect_uri');
+
 // Uber API
-$app->get('uber/redirect_uri','UberController@redirect_uri');
 $app->get('uber/list_product','UberController@list_uber_product');
 $app->get('uber/get_token','UberController@get_token');
 $app->get('uber/current_request','UberController@current_request');
 $app->get('uber/delete_request','UberController@delete_request');
 
-// download data from Barclays API
+// Download data from Barclays API
 $app->get('download_branch_from_barclays','BarclaysController@download_data_branch');
 $app->get('download_atm_from_barclays','BarclaysController@download_data_atm');
 
+// Learning Space
 $app->get('laboratorium','UberController@laboratorium');
 
